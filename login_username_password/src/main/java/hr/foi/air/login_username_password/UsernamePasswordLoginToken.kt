baf3c9b8b3199448f247a12533482779b1369ee3
Dario Ljubas <darioljubas33@gmail.com>
2025-11-05 23:58:14 +0100

    Implement 'login_username_password' module.

--- login_username_password/src/main/java/hr/foi/air/login_username_password/UsernamePasswordLoginToken.kt
+++ login_username_password/src/main/java/hr/foi/air/login_username_password/UsernamePasswordLoginToken.kt
@@ -2,9 +2,11 @@ package hr.foi.air.login_username_password
 
 import hr.foi.air.core.LoginToken
 
-class UsernamePasswordLoginToken : LoginToken {
-    private val authorizers = emptyMap<String, String>()
-
+class UsernamePasswordLoginToken(username: String, password: String) : LoginToken {
+    private val authorizers = mapOf(
+        "username" to username,
+        "password" to password,
+    )
     override fun getAuthorizers(): Map<String, String> {
         return authorizers
     }
